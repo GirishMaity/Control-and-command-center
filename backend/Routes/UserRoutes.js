d1deb2ede2f541c22561db67bf0b0fe4a9f075ba
Girish Maity <girishmaity164@gmail.com>
2022-06-23 22:34:58 +0530

    added registration route

--- backend/Routes/UserRoutes.js
+++ backend/Routes/UserRoutes.js
@@ -9,4 +9,35 @@ router.get("/", (req, res) => {
   res.send("api running");
 });
 
+router.post("/register", async (req, res) => {
+  const { name, email, password, cpassword } = req.body;
+
+  if (!name || !email || !password || !cpassword) {
+    return res.status(400).json({ error: "Invalid Credentials" });
+  } else {
+    if (password === cpassword) {
+      try {
+        const newUser = new User({
+          name,
+          email,
+          password,
+          cpassword,
+        });
+
+        await newUser.save();
+
+        return res.status(201).json({ message: "User created successfully." });
+      } catch (error) {
+        console.log(error.message);
+      }
+    } else {
+      return res.status(400).json({ error: "Invalid Credentials." });
+    }
+  }
+
+  res.json({
+    error: "There was an internal error.",
+  });
+});
+
 module.exports = router;
