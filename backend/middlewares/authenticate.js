c47e306aff93ffa226669ed8dffb11ce75dccc6d
Girish Maity <girishmaity164@gmail.com>
2022-07-14 22:43:56 +0530

    Added authentication middleware

---
+++ backend/middlewares/authenticate.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+const User = require("../Models/UserSchema");
+
+const authenticate = async (req, res, next) => {
+  try {
+    const token = req.cookies.jwtoken;
+    const verify = jwt.verify(token, process.env.SECRET_KEY);
+
+    var rootUser = await User.findOne({
+      _id: verify._id,
+      "tokens.token": token,
+    });
+
+    if (!rootUser) {
+      throw new Error("User now found");
+    }
+
+    req.token = token;
+    req.rootUser = rootUser;
+    req.userId = rootUser._id;
+
+    next();
+  } catch (error) {
+    res.status(400).json({ error: "Unauthorised user." });
+    console.log(error);
+  }
+};
+
+module.exports = authenticate;
